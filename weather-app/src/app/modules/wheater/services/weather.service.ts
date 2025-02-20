a73b0c834f3762726d9dd4e71d063b642bab97ed
Lucas Cordeiro <lucascordeirodelima12@gmail.com>
2025-02-20 15:07:17 -0300

    Reconstrucao do repositorio com weather app completo

---
+++ weather-app/src/app/modules/wheater/services/weather.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+
+  private apiKey = '227b3a7aaecef6b01f82944c492576e2';
+
+  constructor(private http: HttpClient) { }
+
+  getWeatherDatas(cityName: string): Observable<any> {
+    return this.http.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&mode=json&appid=${this.apiKey}`,
+    {});
+  }
+}
